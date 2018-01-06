16d7305020626eb38ddf9e99ab4a9660d0ef12f8
Felipe Rodrigues Michetti <felipe@codecode.com.br>
2018-01-06 21:54:10 -0200

    Fixing Adapter.

--- app/src/main/java/br/com/frmichetti/paymobile/android/adapter/StoreAdapter.java
+++ app/src/main/java/br/com/frmichetti/paymobile/android/adapter/StoreAdapter.java
@@ -5,11 +5,13 @@ package br.com.frmichetti.paymobile.android.adapter;
  */
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -23,19 +25,22 @@ import br.com.frmichetti.paymobile.android.model.compatibility.Product;
  * RecyclerView adapter class to render items
  * This class can go into another separate class, but for simplicity
  */
-public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.MyViewHolder> {
+public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreItemViewHolder> {
     private Context context;
     private List<Product> productList;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView name, price;
+    public class StoreItemViewHolder extends RecyclerView.ViewHolder {
+        public CardView cardView;
         public ImageView thumbnail;
+        public TextView name, price;
 
-        public MyViewHolder(View view) {
+        public StoreItemViewHolder(View view) {
             super(view);
-            name = view.findViewById(R.id.title);
-            price = view.findViewById(R.id.price);
-            thumbnail = view.findViewById(R.id.thumbnail);
+            cardView = view.findViewById(R.id.card_view_store_item);
+            thumbnail = view.findViewById(R.id.imv_thumbnail);
+
+            name = view.findViewById(R.id.tv_name);
+            price = view.findViewById(R.id.tv_price);
         }
     }
 
@@ -46,16 +51,16 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.MyViewHolder
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public StoreItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.store_item_row, parent, false);
 
-        return new MyViewHolder(itemView);
+        return new StoreItemViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, final int position) {
-        final Product product = productList.get(position);
+    public void onBindViewHolder(StoreItemViewHolder holder, final int position) {
+        Product product = productList.get(position);
         holder.name.setText(product.getName());
         holder.price.setText(String.valueOf(product.getPrice()));
 
